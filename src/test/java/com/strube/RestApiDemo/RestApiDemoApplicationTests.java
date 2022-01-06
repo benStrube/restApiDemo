2762548ac2e46dee56b9feef20a13393b72d7812
ben.strube <benjamin.strube@ust.com>
2022-01-05 22:20:47 -0500

    added base files from spring

---
+++ src/test/java/com/strube/RestApiDemo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strube.RestApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
